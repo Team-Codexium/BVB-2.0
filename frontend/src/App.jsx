fa1671f65bffaeb3402793e7d814510daf4ded59
Abhishek Kr <ak96166956@gmail.com>
2025-06-08 17:50:47 +0530

    First Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+import { Button } from "./components/ui/button"
+
+const App = () => {
+  return (
+    <div>
+      Hello
+      <Button variant="outline">Heyy There</Button>
+    </div>
+  )
+}
+
+export default App;
